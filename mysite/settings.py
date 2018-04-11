ca39deefaddb4eb6fdd3e8beed1f5866b530aec2
seungwoonlee <bluecrck@gmail.com>
2018-04-12 02:25:49 +0900

    데이터베이스 셋팅 수정

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+DATABASE_OPTIONS = {'charset': 'utf8'} #추가
+TIME_ZONE = 'Asia/Seoul' #추가
+LANGUAGE_CODE = 'ko-kr' #추가
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
