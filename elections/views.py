19d8c195473b0860def7b9c2e91ae2651205086b
seungwoonlee <bluecrck@gmail.com>
2018-04-12 01:55:19 +0900

    템플릿에 정보 채우기

--- elections/views.py
+++ elections/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Candidate #models에 정의된 Candidate를 import
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world")
+    candidates = Candidate.objects.all()
+    context = {'candidates' : candidates} #context에 모든 후보에 대한 정보를 저장
+    return render(request, 'elections/index.html', context) # context로 html에 모든 후보에 대한 정보를 전달
